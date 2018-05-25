f6b71201496d545c84f9ba7aa47fac41c5bee459
guoleitao <guoleitao@whaty.com>
2018-05-25 12:54:15 +0800

    组合模式包修改

--- src/main/java/com/glt/designpattern/composite/InitMain.java
+++ src/main/java/com/glt/designpattern/composite/InitMain.java
@@ -1,10 +1,13 @@
-package com.glt.designpattern.component;
+package com.glt.designpattern.composite;
 
 public class InitMain {
     public static void main(String[] args) {
         /**
+         * 组合模式
          * 允许你将对象组合成树形结构来表现“整体/部分”层次结构，组合能让客户以一致的方式处理个别对象以及对象组合。
          *
+         *
+         * 组合模式客户程序与复杂元素的内部结构，从而是客户程序可以想处理简单元素一样处理复杂对象。
          */
 
 
