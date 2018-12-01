b9c4e40c775d3913f42e32a70f22dd89e24bcd6a
leitao_guo@126.com <glt798693>
2018-12-01 21:19:08 +0800

    责任链模式InitMain

---
+++ src/main/java/com/glt/designpattern/ChainOfResponsibility/InitMain.java
@@ -0,0 +1,13 @@
+package com.glt.designpattern.ChainOfResponsibility;
+
+public class InitMain {
+    public static void main(String[] args) {
+        /**
+         * 责任链模式
+         *  将发送者与接收者解耦，为请求创建了一个接收者对象的链，使链上的所有对象都可能处理这个请求
+         *
+         * 特点
+         *  可以通过改变链内的成员或改变他们的顺次，允许动态的新增或删除责任
+         */
+    }
+}
