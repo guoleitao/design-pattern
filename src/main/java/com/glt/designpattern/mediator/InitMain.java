5d9a95e5787b0ea864ed0b38e879ad1111be41f3
leitao_guo@126.com <glt798693>
2018-12-01 22:43:15 +0800

    中介者模式InitMain

---
+++ src/main/java/com/glt/designpattern/mediator/InitMain.java
@@ -0,0 +1,17 @@
+package com.glt.designpattern.mediator;
+
+public class InitMain {
+    public static void main(String[] args) {
+        /**
+         * 中介者模式
+         *  使用中介者对象使其他对象之间相互解耦，每个对象只需要向中介者对象发出请求和接受请求即可
+         *
+         * 特点
+         *  增加对象的复用性
+         *  通过将控制逻辑集中，简化了系统的维护
+         *  让对象之间传递消息变的更加简单
+         *  如果系统设计不当， 会造成中介者对象变的过于复杂
+         *
+         */
+    }
+}
